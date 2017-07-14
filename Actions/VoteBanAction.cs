6ae70e27a4c9967f61b921a060343a349e75679d
canu1337 <treisska@gmail.com>
2017-07-14 16:48:29 +0200

    Add basic message sending part 2

--- Actions/VoteBanAction.cs
+++ Actions/VoteBanAction.cs
@@ -11,11 +11,10 @@ namespace RepubliqueBot.Actions
         {
             this.message = msg;
         } 
-
         void IAction.execute()
         {
             TelegramService service = new TelegramService();
-            service.SendMessage(message.Chat.Id.ToString(), ">insinuant que c'est implémenté");
+            service.SendMessage(message.Chat.Id, ">insinuant que c'est implémenté");
         }
     }
 
--- TelegramService.cs
+++ TelegramService.cs
@@ -1,32 +1,31 @@
 using System;
 using System.Net.Http;
 using System.Text;
+using Newtonsoft.Json;
 using RepubliqueBot.Models;
 
 namespace RepubliqueBot {
 
     public class TelegramService {
         private static string telegramURL = "https://api.telegram.org/bot{0}/{1}";
-        private string token = "";
+        private string token = "382765279:AAEnglUFw0z0ExA5HrI9VwSKaLq2DM3t68w";
         private HttpClient httpClient;
 
         public TelegramService () {
             this.httpClient = new HttpClient();
         }
-        public void SendMessage (string group, string text){
+        public void SendMessage (int group, string text){
             SendMessage msg = new SendMessage() { ChatId = group, Text = text };
-            StringContent content = new StringContent(msg.ToString(), Encoding.UTF8, "application/json");
-            var r = httpClient.PostAsync(this.getURL(TelegramMethod.SendMessage), content);
+            string json = JsonConvert.SerializeObject(msg, Newtonsoft.Json.Formatting.None, 
+                            new JsonSerializerSettings { 
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            var r = httpClient.PostAsync(this.getURL("sendMessage"), content).Result;
         }
 
-
-        private string getURL (TelegramMethod method) {
-            string meth;
-            switch (method){
-                case TelegramMethod.SendMessage : meth = "sendMessage"; break;
-                default : meth = ""; break;
-            }
-            return String.Format(TelegramService.telegramURL, this.token, meth);
+        private string getURL (string method) {
+            return String.Format(TelegramService.telegramURL, this.token, method);
         }
     }
 }
